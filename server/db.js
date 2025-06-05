5cb48089d6298c277cc776e643333e6b3251e970
liamma06 <liam.jbr.ma@gmail.com>
2025-06-05 11:43:27 -0400

    added mongoose connection

--- server/db.js
+++ server/db.js
@@ -1,15 +1,43 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+import mongoose from 'mongoose';
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    // Exit process with failure
-    process.exit(1);
+let cached = global.mongoose;
+
+if(!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB(){
+  //if connection already exists, return it
+  if(cached.conn){
+    return cached.conn;
   }
-};
 
-module.exports = connectDB;
+  //if no connection, create a new one
+  if(!cached.promise){
+    const opts ={
+      bufferCommands:false,
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts)
+      .then((mongoose) => {
+        console.log("MongoDB connected");
+        return mongoose;
+      })
+      .catch((error) => {
+        console.error("MongoDB connection error:", error);
+        throw error;
+      });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+
+}
+
+export default connectDB;
