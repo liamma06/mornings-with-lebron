bdabce71e988178d0bcf2230936e5cd4e3262b7f
liamma06 <liam.jbr.ma@gmail.com>
2025-06-06 16:06:18 -0400

    serverside fixes

--- server/app.js
+++ server/app.js
@@ -1,33 +1,28 @@
-const express = require('express')
-const connectDB = require('./db')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-//connectDB()
 
 const cors = require('cors');
 app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3001', 
-    'https://your-vercel-app.vercel.app' // Add your Vercel URL here
+    'https://mornings-with-lebron.vercel.app/'
   ],
   credentials: true
 }));
 
-
-
 app.get('/', (req, res) => {
-  console.log("hellow world")
-  res.send("Hello world")
-})
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Example app listening on port ${PORT}`)
+  res.send("Mornings with LeBron API is running!");
 });
 
-//reflection route
-const userRouter = require('./routes/reflection')
-app.use('/reflection', userRouter)
+// Reflection route
+const userRouter = require('./routes/reflection');
+app.use('/reflection', userRouter);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`);
+});
